17ee3350c6aaf8e7573edb4c76e7610a674c6a81
leelakiran0 <76879868+leelakiran0@users.noreply.github.com>
2021-01-02 23:59:38 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! changes quickly"); 
+        System.out.println("Hello, World! changes"); 
     }
 }
